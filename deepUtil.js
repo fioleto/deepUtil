5947428791ec2d6bc17ae024cbcff7bd73adbfc8
fioleto <fioleto@protonmail.com>
2020-04-02 17:16:18 +0900

    init

---
+++ deepUtil.js
@@ -0,0 +1,200 @@
+"use strict";
+
+class deepUtil{
+    constructor(){}
+
+    static merge(...targets){
+        let mergedObject = {};
+        const merge = function(source, mergedObject){
+            let keyArray = Object.getOwnPropertyNames(source);
+            for(let prop of keyArray){
+                if(
+                    source.hasOwnProperty(prop) &&
+                    source[prop] !== null
+                ){
+                    if(typeof source[prop] === "object"){
+                        if(!mergedObject.hasOwnProperty(prop))
+                            mergedObject[prop] = {};
+
+                        merge(source[prop], mergedObject[prop]);
+                    }else if(typeof source[prop] !== "function")
+                        mergedObject[prop] = source[prop];
+                }
+            }
+        };
+
+        for(let target of targets)
+            merge(target, mergedObject);
+
+        return mergedObject;
+    }
+
+    static copy(source){
+        let parsedString = ``;
+        try{
+            parsedString = JSON.stringify(source);
+        }catch(err){
+            throw err;
+        }
+
+        return JSON.parse(parsedString);
+    }
+
+    static freeze(source){
+        Object.freeze(source);
+
+        let keyArray = Object.getOwnPropertyNames(source);
+        for(let prop of keyArray){
+            if(
+                source.hasOwnProperty(prop) &&
+                source[prop] !== null &&
+                (typeof source[prop] === "object" || typeof source[prop] === "function") &&
+                !Object.isFrozen(source[prop])
+            ){
+                deepUtil.freeze(source[prop]);
+            }
+        }
+
+        return source;
+    }
+
+    static check(source, types, repeat){
+        let result = {
+            error: false,
+            notFound: {},
+            typeError: {}
+        };
+    
+        let repeatRegex = (repeat instanceof RegExp) ? repeat : /__$/;
+    
+        const typeCheck = function(type, value){
+            const typeList = {
+                isNumber: function(value){
+                    return (typeof value === 'number');
+                },
+                isString: function(value){
+                    return (typeof value === 'string');
+                },
+                isBoolean: function(value){
+                    return (typeof value === 'boolean');
+                },
+                isNull: function(value){
+                    return (value === null);
+                },
+                isUndefined: function(value){
+                    return (typeof value === 'undefined');
+                },
+                isNaN: function(value){
+                    return Number.isNaN(value);
+                }
+            };
+    
+            const definedType = [
+                "number",
+                "string",
+                "boolean",
+                "null",
+                "undefined",
+                "nan"
+            ];
+    
+            let definedTypeAndArray = [].concat(
+                definedType,
+                definedType.map(
+                    typeString => {
+                        return `${typeString}[]`;
+                    }
+                )
+            );
+    
+            if(!definedTypeAndArray.includes(type))
+                return false;
+    
+            const functionName = `is${type.charAt(0).toUpperCase()}${type.slice(1)}`.replace(/\[\]$/, '');
+    
+            if(/.*\[\]$/.test(type)){
+                if(!Array.isArray(value))
+                    return false;
+                else{
+                    for(let currentValue of value){
+                        if(!typeList[functionName](currentValue))
+                            return false;
+                    }
+    
+                    return true;
+                }
+            }else
+                return typeList[functionName](value);
+        }
+    
+        const check = function(types, source, notFound, typeError){
+            let keyArray = Object.getOwnPropertyNames(types);
+    
+            for(let prop of keyArray){
+    
+                if(repeatRegex.test(prop)){
+                    let keyName = String(prop).replace(repeatRegex, '');
+    
+                    if(source.hasOwnProperty(keyName)){
+    
+                        if(
+                            Array.isArray(source[keyName])
+                        ){
+                            typeError[keyName] = [];
+                            notFound[keyName] = [];
+    
+                            for(let unit of source[keyName]){
+                                let recursiveResult = deepUtil.check(unit, types[prop], repeatRegex);
+                                if(recursiveResult.error){
+                                    typeError[keyName].push(recursiveResult.typeError);
+                                    notFound[keyName].push(recursiveResult.notFound);
+                                    result.error = true;
+                                }else{
+                                    typeError[keyName].push(recursiveResult.typeError);
+                                    notFound[keyName].push(recursiveResult.notFound);
+                                }
+                            }
+                        }else{
+                            typeError[keyName] = true;
+                            result.error = true;
+                        }
+                    }else{
+                        notFound[keyName] = true;
+                        result.error = true;
+                    }
+                }else{
+                    if(source.hasOwnProperty(prop)){
+                        if(
+                            typeof types[prop] === "object" &&
+                            types[prop] !== null
+                        ){
+                            notFound[prop] = {};
+                            typeError[prop] = {};
+    
+                            check(types[prop], source[prop], notFound[prop], typeError[prop]);
+                        }else if(typeof types[prop] !== "function"){
+                            let isDesignatedType = typeCheck(types[prop], source[prop]);
+                            notFound[prop] = false;
+    
+                            if(isDesignatedType)
+                                typeError[prop] = false;
+                            else{
+                                typeError[prop] = true;
+                                result.error = true;
+                            }
+                        }
+                    }else{
+                        notFound[prop] = true;
+                        result.error = true;
+                    }
+                }
+            }
+        }
+    
+        check(types, source, result.notFound, result.typeError);
+    
+        return result;
+    }
+}
+
+module.exports = deepUtil;
---
+++ tests/check.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env node
+
+const deepUtil = require("../deepUtil.js");
+
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+const testObject = {
+    squadName: "Super hero squad",
+    homeTown: "Metro City",
+    formed: 2016,
+    secretBase: "Super tower",
+    active: true,
+    members: [
+        {
+            name: "Molecule Man",
+            age: 29,
+            secretIdentity: "Dan Jukes",
+            powers: [
+                "Radiation resistance",
+                "Turning tiny",
+                "Radiation blast"
+            ]
+        },
+        {
+            name: "Madame Uppercut",
+            age: 39,
+            secretIdentity: "Jane Wilson",
+            powers: [
+                "Million tonne punch",
+                "Damage resistance",
+                "Superhuman reflexes"
+            ]
+        },
+        {
+            name: "Eternal Flame",
+            age: 1000000,
+            secretIdentity: "Unknown",
+            powers: [
+                "Immortality",
+                "Heat Immunity",
+                "Inferno",
+                "Teleportation",
+                "Interdimensional travel"
+            ]
+        }
+    ]
+};
+
+const OK1 = deepUtil.check(
+    testObject,
+    {
+        squadName: "string",
+        homeTown: "string",
+        formed: "number",
+        secretBase: "string",
+        active: "boolean",
+        members__: {
+            name: "string",
+            age: "number",
+            secretIdentity: "string",
+            powers: "string[]"
+        } 
+    }
+);
+
+const OK2 = deepUtil.check(
+    testObject,
+    {
+        squadName: "string",
+        homeTown: "string",
+        formed: "number",
+        secretBase: "string",
+        active: "boolean",
+        members$: {
+            name: "string",
+            age: "number",
+            secretIdentity: "string",
+            powers: "string[]"
+        } 
+    },
+    /\$$/ // repeat RegExp
+);
+
+const NG = deepUtil.check(
+    testObject,
+    {
+        squadName: "number", // Type error.
+        homeTown: "string",
+        formed: "number",
+        secretBase: "string",
+        active: "boolean",
+        members__: {
+            name: "string",
+            age: "number",
+            // secretIdentity: "string", // Not found.
+            powers: "string[]"
+        } 
+    }
+);
+
+console.log(
+    OK1.error, // false,
+    OK2.error, // false
+    NG.error  // true
+);
